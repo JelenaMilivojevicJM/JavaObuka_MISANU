7d337edc1caca9383c6f76c0886038e73915d4b8
JelenaMilivojevicJM <jelenamilivojevic994@gmail.com>
2023-05-16 13:46:31 +0200

    Kolekcije
    Predavanje 26 + deo prakse:
    - ArrayList,
    - Iterface Iterator,
    - LinkedList,
    - ListIterator
    - List Kolekcija
    - Map Kolekcija
    - Kolekcije + OOP koncept.

---
+++ 8.1_Kolekcije/Map_Primer2_OOP/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author jelen
+ *
+ */
+module Map_Primer2_OOP {
+}
---
+++ 8.1_Kolekcije/Map_Primer2_OOP/src/paket_map/Main.java
@@ -0,0 +1,38 @@
+package paket_map;
+public class Main {
+
+	public static void main(String[] args) {
+		// Kreiranje objekata klase Ucenik - 2 učenika
+		Ucenik ucenik1 = new Ucenik("Mika", 15);
+		Ucenik ucenik2 = new Ucenik("Zika", 16);
+
+		// Dodavanje ocena za svakog učenika
+		ucenik1.dodajOcenu("Matematika", 9);
+		ucenik1.dodajOcenu("Fizika", 8);
+		ucenik1.dodajOcenu("Hemija", 7);
+		ucenik1.dodajOcenu("Engleski", 10);
+
+		ucenik2.dodajOcenu("Matematika", 10);
+		ucenik2.dodajOcenu("Fizika", 7);
+		ucenik2.dodajOcenu("Hemija", 6);
+		ucenik2.dodajOcenu("Engleski", 9);
+
+		// Prikazivanje ocena
+		System.out.println("Ocene učenika " + ucenik1.getIme() + ":");
+		ucenik1.ispisOcene();
+		System.out.println("Ocene učenika " + ucenik2.getIme() + ":");
+		ucenik2.ispisOcene();
+		
+		//Brisanje nekih ocena iz kolekcije ocena
+		 // Uklanjanje ocene
+		ucenik1.izbrisiOcenu("Fizika");
+		ucenik2.izbrisiOcenu("Hemija");
+        
+        // Prikazivanje ocena nakon uklanjanja
+        System.out.println("Ocene učenika " + ucenik1.getIme() + " nakon uklanjanja ocene iz Fizike:");
+        ucenik1.ispisOcene();
+        System.out.println("Ocene učenika " + ucenik2.getIme() + " nakon uklanjanja ocene iz Hemije:");
+        ucenik2.ispisOcene();
+	}
+
+}
